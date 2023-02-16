253b964766b09c501a1114ce282a3e5210203be9
gazi_fayaz <gazifayaz.16694@gmail.com>
2023-02-16 16:17:34 +0600

    delete unnecessary files

---
+++ productivity-web-ap/src/chakra/theme.ts
@@ -0,0 +1,24 @@
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react"
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#d1453b",
+    },
+    fonts: {
+        body: 'Open Sans, sans-serif'
+    }, 
+    styles: {
+        global: () =>({
+            body: {
+                bg: "Gray.50"
+            }
+        })
+    }
+  },
+})
