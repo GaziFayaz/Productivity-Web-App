370b46f3b85274734e0d13c44e67a2848af3cf33
Sakibfootball <sakibhaider20@gmail.com>
2023-02-17 12:34:26 +0600

    sidebar without Mobile_F

--- productivity-web-ap/src/components/Layout/Layout.tsx
+++ productivity-web-ap/src/components/Layout/Layout.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Sidebar from "../Sidebar/Sidebar";
+import {
+  Flex,
+  Heading,
+  Avatar,
+  AvatarGroup,
+  Text,
+  Icon,
+  IconButton,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Divider,
+  Link,
+  Box,
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 
 interface LayoutProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const Layout:React.FC<LayoutProps> = ({ children }) => {
-    
-    return (
-        <>
-        {<Navbar />}
-        <main>{children}</main>
-        </>
-    )
-}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      {<Navbar />}
+      <Flex h="100vh" flexDir="row" overflow="hidden" maxW="2000px">
+        <Sidebar />
+        
+      </Flex>
+      <main>{children}</main>
+    </>
+  );
+};
 export default Layout;
