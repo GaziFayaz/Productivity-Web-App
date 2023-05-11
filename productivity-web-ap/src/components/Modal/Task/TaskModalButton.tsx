673d1ab097e0f7a2b2ecd2f960349dd612113f95
Sakibfootball <sakibhaider20@gmail.com>
2023-05-11 18:07:51 +0600

    task modal date and prority insertion, label and reminder deletion

--- productivity-web-ap/src/components/Modal/Task/TaskModalButton.tsx
+++ productivity-web-ap/src/components/Modal/Task/TaskModalButton.tsx
@@ -4,12 +4,16 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Menu,
+  MenuButton,
+  MenuList,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalOverlay,
+  Select,
   Spacer,
   Stack,
   Text,
@@ -18,7 +22,9 @@ import {
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TaskCategory from "./TaskCategory";
-
+import DateTimePicker from "react-datetime-picker/dist/entry.nostyle";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 import { auth, db } from "@/firebase/clientApp";
 import { AddIcon } from "@chakra-ui/icons";
 import {
@@ -31,7 +37,7 @@ import {
   Transaction,
   updateDoc,
 } from "firebase/firestore";
-import Router from "next/router"
+import Router from "next/router";
 
 const TaskModalButton: React.FC = (props: any) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -43,12 +49,20 @@ const TaskModalButton: React.FC = (props: any) => {
   const [label, setLabel] = useState("read");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState("Inbox");
 
   const handleChangeTask = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTaskName(event.target.value);
   };
   const handleChangeDesc = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDesc(event.target.value);
+  };
+  const handleChangePriority = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPriority(event.target.value);
+    console.log(priority);
+  };
+  const handleChangecategtory = (e: any) => {
+    setCategory(e.target.value);
   };
 
   // const handleClose = () => {
@@ -62,33 +76,33 @@ const TaskModalButton: React.FC = (props: any) => {
     if (error) setError("");
     setLoading(true);
     try {
-        // Create the task document in firestore
-        // const taskDocRef= firestore.collection("tasks").doc();
-        
-        const data = {
-          creatorId: user?.uid,
-          createdAt: serverTimestamp(),
-          taskName: taskName,
-          desc: desc,
-          dueDate: dueDate,
-          priority: priority,
-          label: label,
-          isAssigned: false,
-          assignedTo: null,
-          isInProject: false,
-          fromProject: null,
-        };
-        const taskDocRef = doc(collection(db, "tasks"));
-        await setDoc(taskDocRef, data);
-        const taskId = taskDocRef.id;
-        if (props.sectionId) {
-          const secRef = doc(db, "sections", props.sectionId);
-          await updateDoc(secRef, {
-            tasks: arrayUnion(taskId),
-          });
-        }
-        onClose();
-        Router.reload()
+      // Create the task document in firestore
+      // const taskDocRef= firestore.collection("tasks").doc();
+
+      const data = {
+        creatorId: user?.uid,
+        createdAt: serverTimestamp(),
+        taskName: taskName,
+        desc: desc,
+        dueDate: dueDate,
+        priority: priority,
+        label: label,
+        isAssigned: false,
+        assignedTo: null,
+        isInProject: false,
+        fromProject: null,
+      };
+      const taskDocRef = doc(collection(db, "tasks"));
+      await setDoc(taskDocRef, data);
+      const taskId = taskDocRef.id;
+      if (props.sectionId) {
+        const secRef = doc(db, "sections", props.sectionId);
+        await updateDoc(secRef, {
+          tasks: arrayUnion(taskId),
+        });
+      }
+      onClose();
+      Router.reload();
     } catch (error: any) {
       console.log("handleCreateTask error", error);
       setError(error.message);
@@ -152,10 +166,36 @@ const TaskModalButton: React.FC = (props: any) => {
               />
             </FormControl>
             <Stack direction="row" spacing={4} align="center" paddingTop="4">
-              <Button size="xs">Due Date</Button>
-              <Button size="xs">Priority</Button>
-              <Button size="xs">Reminders</Button>
-              <Button size="xs">Label</Button>
+              <Button size="xs">
+                <Menu isLazy>
+                  <MenuButton
+                    fontSize="xs"
+                    fontWeight="semibold"
+                    cursor={"pointer"}
+                  >
+                    <Flex flexDir={"row"}>{dueDate.toLocaleString()}</Flex>
+                  </MenuButton>
+
+                  <MenuList>
+                    <DateTimePicker value={dueDate} onChange={setDueDate} />
+                  </MenuList>
+                </Menu>
+              </Button>
+
+              <Button size="xs">
+                <Select
+                  placeholder="priority"
+                  value={priority}
+                  onChange={handleChangePriority}
+                  size="xs"
+                  variant="unstyled"
+                >
+                  <option value="red.500">1</option>
+                  <option value="green.500">2</option>
+                  <option value="yellow.500">3</option>
+                  <option value="gray.500">4</option>
+                </Select>
+              </Button>
             </Stack>
             <Text fontSize={"9pt"} color="red" pt={1}>
               {error}
@@ -163,7 +203,18 @@ const TaskModalButton: React.FC = (props: any) => {
           </ModalBody>
 
           <ModalFooter>
-            <TaskCategory />
+            <Button size="xs">
+              <Select
+                value={category}
+                onChange={handleChangecategtory}
+                size="xs"
+                variant="unstyled"
+              >
+                <option value="Inbox">Inbox</option>
+                <option value="Education">Education</option>
+                <option value="Home">Home</option>
+              </Select>
+            </Button>
             <Spacer />
             <Flex>
               <Button
--- productivity-web-ap/src/components/Modal/Task/TaskModalIcon.tsx
+++ productivity-web-ap/src/components/Modal/Task/TaskModalIcon.tsx
@@ -15,11 +15,17 @@ import {
   useDisclosure,
   Text,
   Box,
+  Menu,
+  MenuButton,
+  MenuList,
+  Select,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TaskCategory from "./TaskCategory";
-
+import DateTimePicker from "react-datetime-picker/dist/entry.nostyle";
+import "react-calendar/dist/Calendar.css";
+import "react-clock/dist/Clock.css";
 import { auth, db } from "@/firebase/clientApp";
 import { AddIcon } from "@chakra-ui/icons";
 import { collection, doc, serverTimestamp, setDoc } from "firebase/firestore";
@@ -29,17 +35,25 @@ const TaskModalIcon: React.FC = () => {
   const [user] = useAuthState(auth);
   const [taskName, setTaskName] = useState("Dummy");
   const [desc, setDesc] = useState("This is a dummy description");
-  const [dueDate, setDueDate] = useState("");
+  const [dueDate, setDueDate] = useState(new Date());
   const [priority, setPriority] = useState("1");
   const [label, setLabel] = useState("read");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState("Inbox");
 
   const handleChangeTask = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTaskName(event.target.value);
   };
   const handleChangeDesc = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDesc(event.target.value);
+  };
+  const handleChangePriority = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPriority(event.target.value);
+    console.log(priority);
+  };
+  const handleChangecategtory = (e: any) => {
+    setCategory(e.target.value);
   };
 
   // const handleClose = () => {
@@ -119,10 +133,36 @@ const TaskModalIcon: React.FC = () => {
               />
             </FormControl>
             <Stack direction="row" spacing={4} align="center" paddingTop="4">
-              <Button size="xs">Due Date</Button>
-              <Button size="xs">Priority</Button>
-              <Button size="xs">Reminders</Button>
-              <Button size="xs">Label</Button>
+              <Button size="xs">
+                <Menu isLazy>
+                  <MenuButton
+                    fontSize="xs"
+                    fontWeight="semibold"
+                    cursor={"pointer"}
+                  >
+                    <Flex flexDir={"row"}>{dueDate.toLocaleString()}</Flex>
+                  </MenuButton>
+
+                  <MenuList>
+                    <DateTimePicker value={dueDate} onChange={setDueDate} />
+                  </MenuList>
+                </Menu>
+              </Button>
+
+              <Button size="xs">
+                <Select
+                  placeholder="priority"
+                  value={priority}
+                  onChange={handleChangePriority}
+                  size="xs"
+                  variant="unstyled"
+                >
+                  <option value="red.500">1</option>
+                  <option value="green.500">2</option>
+                  <option value="yellow.500">3</option>
+                  <option value="gray.500">4</option>
+                </Select>
+              </Button>
             </Stack>
             <Text fontSize={"9pt"} color="red" pt={1}>
               {error}
@@ -130,7 +170,18 @@ const TaskModalIcon: React.FC = () => {
           </ModalBody>
 
           <ModalFooter>
-            <TaskCategory />
+            <Button size="xs">
+              <Select
+                value={category}
+                onChange={handleChangecategtory}
+                size="xs"
+                variant="unstyled"
+              >
+                <option value="Inbox">Inbox</option>
+                <option value="Education">Education</option>
+                <option value="Home">Home</option>
+              </Select>
+            </Button>
             <Spacer />
             <Flex>
               <Button
