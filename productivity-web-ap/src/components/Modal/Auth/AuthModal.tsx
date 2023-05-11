92881cbe5f734fcffade9da33f508062ed34bd0a
gazi_fayaz <gazifayaz.16694@gmail.com>
2023-05-12 04:44:02 +0600

    Add functionailty of adding user to users database if already doesn't exist in users database

--- productivity-web-ap/src/components/Modal/Auth/AuthModal.tsx
+++ productivity-web-ap/src/components/Modal/Auth/AuthModal.tsx
@@ -14,16 +14,20 @@ import {
   Text,
   useFocusEffect,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/clientApp";
+import { auth, db } from "@/firebase/clientApp";
 import ResetPassword from "./ResetPassword";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { AiOutlineConsoleSql } from "react-icons/ai";
+import { Router } from "next/router";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
   const [user, loading, error] = useAuthState(auth);
+  const [checkUser, setCheckUser] = useState(false)
 
   const handleClose = () => {
     setModalState((prev) => ({
@@ -32,10 +36,59 @@ const AuthModal: React.FC = () => {
     }));
   };
 
-  useEffect(() => {
-    if (user) handleClose();
-    console.log("user", user);
+  useEffect( () => {
+    if (user) {
+      console.log("user", user);
+      addUser()
+    }
   }, [user]);
+
+  useEffect ( () => {
+    if(checkUser) {
+      handleClose();
+    }
+  }, [checkUser])
+
+  const addUser = async () => {
+    if(user){
+      const userDocRef = doc(db, "users", user.uid)
+      const userSnap = await getDoc(userDocRef)
+      if(!userSnap.exists()){
+
+        // const inboxSection = {
+
+        // }
+
+        // const inboxProject = {
+        //   creatorId: user?.uid,
+        //   createdAt: serverTimestamp(),
+        //   projectName:"inbox",
+        //   color:"1",
+        //   colorValue: "1",
+        //   numberOfMembers: 1,
+        //   members: [user?.uid],
+        //   numberOfSections: 1,
+        //   // sections: [section]
+        // }
+        const displayName = user.displayName? user.displayName : user.email
+        const userData= {
+          email: user.email,
+          name: displayName,
+          projects: [],
+        }
+        await setDoc(userDocRef, userData)
+        .then(() => {
+          setCheckUser(true)
+        })
+        .catch(() => {
+          console.log("Adding New User to Database Failed")
+        })
+      }
+      else setCheckUser(true)
+    }
+    
+    
+  }
 
   return (
     <>
