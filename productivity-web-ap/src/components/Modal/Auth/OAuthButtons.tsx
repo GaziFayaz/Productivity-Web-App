744f96838584f12915fb314a0b5c20f4dce4ec90
gazi_fayaz <gazifayaz.16694@gmail.com>
2023-05-15 08:33:30 +0600

    remved a line

--- productivity-web-ap/src/components/Modal/Auth/OAuthButtons.tsx
+++ productivity-web-ap/src/components/Modal/Auth/OAuthButtons.tsx
@@ -16,7 +16,6 @@ const OAuthButtons: React.FC = () => {
         <Image src="/images/googlelogo.png" height="20px" mr={4} />
         Continue with Google
       </Button>
-      <Button variant={"oauth"}>Some other Provider</Button>
       {error && <Text>{error.message}</Text>}
     </Flex>
   );
