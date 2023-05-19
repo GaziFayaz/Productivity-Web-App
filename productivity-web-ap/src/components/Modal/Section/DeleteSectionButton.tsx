2dc9a917b56d0951eab139c71a284f6568f8ed12
gazi_fayaz <gazifayaz.16694@gmail.com>
2023-05-19 10:34:21 +0600

    Delete Section Button

---
+++ productivity-web-ap/src/components/Modal/Section/DeleteSectionButton.tsx
@@ -0,0 +1,34 @@
+import { db } from "@/firebase/clientApp";
+import { Icon, Text } from "@chakra-ui/react";
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+
+const DeleteSectionButton = (section) => {
+  const router = useRouter();
+
+  const deleteTask = async () => {
+       console.log(section)
+    section.section.tasks.map(async (task) => {
+        await deleteDoc(doc(db, "tasks", task));
+    });
+    await deleteDoc(doc(db, "sections", section.sectionId)).then( async() => {
+        const projectRef = doc(db, "projects", section.projectId)
+        await updateDoc(projectRef, {
+        sections: arrayRemove(section.sectionId),
+      })
+    })
+    router.reload();
+  };
+  return (
+    <Text
+      color={"red"}
+      width={"full"}
+      cursor="pointer"
+      onClick={deleteTask}
+    >Delete</Text>
+  );
+};
+
+export default DeleteSectionButton;
